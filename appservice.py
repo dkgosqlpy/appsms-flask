5d186c0a6c33a050748d02f9e1413d87b51f6eea
d k s <dhananjayksharma@gmail.com>
2021-09-25 18:54:40 +0530

    added Canada

--- appservice.py
+++ appservice.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Flask
 app = Flask(__name__)
-_countryList = [{'name': 'Sri Lanka', 'id': 88, 'continent':'Asia'}, {'name': 'India', 'id': 99, 'continent':'Asia'}, {'name': 'USA', 'id': 1, 'continent':'America'}, {'name': 'Japan', 'id': 11, 'continent':'Asia'},
+_countryList = [{'name': 'Canada', 'id': 1, 'continent':'North America'}, {'name': 'Sri Lanka', 'id': 88, 'continent':'Asia'}, {'name': 'India', 'id': 99, 'continent':'Asia'}, {'name': 'USA', 'id': 1, 'continent':'America'}, {'name': 'Japan', 'id': 11, 'continent':'Asia'},
                 {'name': 'German', 'id': 15, 'continent':'Europe'}, {'name': 'Russia', 'id': 66, 'continent':'Asia'}, {'name': 'Nepal', 'id': 145, 'continent':'Asia'}]
 
 @app.route('/')
@@ -29,4 +29,4 @@ def client():
   return 'Hello World!'
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=int("5000"), debug=True)#debug = True
+   app.run(host="0.0.0.0", port=int("5000"), debug=False)#debug = True
